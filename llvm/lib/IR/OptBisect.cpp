186471e7c37d7d833584a4a5d3104d8b48f693fc
DianQK <dianqk@dianqk.net>
2023-10-12 17:45:03 +0800

    Only consider key functions

--- llvm/lib/IR/OptBisect.cpp
+++ llvm/lib/IR/OptBisect.cpp
@@ -43,6 +43,10 @@ bool OptBisect::shouldRunPass(const StringRef PassName,
                               StringRef IRDescription) {
   assert(isEnabled());
 
+  bool IsKeyFunction = IRDescription == "_RINvMs4_NtNtNtCs8zlSoIwVZ9h_15rustc_mir_build4thir7pattern15deconstruct_patNtB6_11Constructor5splitINtNtNtNtCse4hYLEadXti_4core4iter8adapters3map3MapIB1A_INtNtNtB1I_5slice4iter4IterNtNtB8_10usefulness8PatStackENCNvMs2_B2V_NtB2V_6Matrix5heads0ENvMs7_B6_NtB6_16DeconstructedPat4ctorEEBc_";
+  IsKeyFunction = IsKeyFunction || IRDescription == "_RNCINvMs4_NtNtNtCs8zlSoIwVZ9h_15rustc_mir_build4thir7pattern15deconstruct_patNtB8_11Constructor5splitINtNtNtNtCse4hYLEadXti_4core4iter8adapters3map3MapIB1C_INtNtNtB1K_5slice4iter4IterNtNtBa_10usefulness8PatStackENCNvMs2_B2X_NtB2X_6Matrix5heads0ENvMs7_B8_NtB8_16DeconstructedPat4ctorEEs0_0Be_";
+  if (!IsKeyFunction)
+      return true;
   int CurBisectNum = ++LastBisectNum;
   bool ShouldRun = (BisectLimit == -1 || CurBisectNum <= BisectLimit);
   printPassMessage(PassName, CurBisectNum, IRDescription, ShouldRun);
